1336be2bb86882c49e01d3888eaa163db93ccc57
PharrellWANG <wzxnuaa@gmail.com>
2018-08-16 11:04:05 +0800

    now the 2_average.py is able to skip duplicated documents

--- 2_average.py
+++ 2_average.py
@@ -4,9 +4,19 @@ from utils import average_last_column, scan_tree
 
 HOME = os.environ['HOME']
 PREFIX = '/Documents/Data/PercentAnalysis/CSV/001CommaRemoved/'
+# No Duplication Ref
+NODUPREF = '/Documents/Data/PercentAnalysis/CSV/NoDuplicationReferences/'
 
 list_of_input_files = scan_tree(HOME + PREFIX)
+list_of_no_dup_ref_files = scan_tree(HOME + NODUPREF)
 
 
 for x in range(len(list_of_input_files)):
-    average_last_column(list_of_input_files[x])
+    file_name = list_of_input_files[x].split('/')[-1]
+    skip_flag = False
+    for y in range(len(list_of_no_dup_ref_files)):
+        if list_of_no_dup_ref_files[y].split('/')[-1] == file_name:
+            skip_flag = True
+            break
+    if not skip_flag:
+        average_last_column(list_of_input_files[x])
